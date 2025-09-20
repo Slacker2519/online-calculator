b1df90522421582cf8c9846ba70e5df965d2ae05
Slacker2519 <tienduongeng2001@gmail.com>
2025-09-20 09:41:57 +0000

    Style the page a little bit with css

---
+++ script.js
@@ -0,0 +1,11 @@
+const keysNumber = 20;
+const calculatorDiv = document.querySelector('#calculator');
+const screenDiv = calculatorDiv.querySelector('#screen');
+const keysDiv = calculatorDiv.querySelector('#keys');
+
+function createKeys() {
+    for (let i = 0; i < keysNumber; i++) {
+        let button = document.createElement('button');
+        
+    }
+}
